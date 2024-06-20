9893b59b6a9398dcfb3644465c086d8b9d79c07b
ChemEngBear <ehdgus5655@gmail.com>
2024-06-20 19:33:08 +0900

    깃허브에 리엑트 앱 업로드하기

---
+++ Desktop/KDT_자료_데이터/reactWith김한길/movie_app_2024/src/App.js
@@ -0,0 +1,51 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+// import 'Potato'; 
+
+// function Potato;
+// retu
+
+// function App( {
+
+// }  
+// 	return (
+//     <div> 
+//       <h1>hello</h1>
+//     </div>
+
+
+  // export default App; /
+
+
+ function Food(props){
+    console.log(props);}
+    return <h3>  I tdurfmf tjdi ㅊ첻드 이만 가신것인지 {{props.faw}} </h3>
+
+
+
+    
